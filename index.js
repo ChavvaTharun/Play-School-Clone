8a2aa5d3e003a75aa938d886cbd56e37307ed546
chavvatharun <tharunchintu100@gmail.com>
2024-01-04 14:05:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+// JS for Nav
+
+document.getElementById('demo-link').onmouseover=function(){
+  // document.getElementById('demo-modal').style.height='150px';
+  document.getElementById('demo-modal').style.display="block";
+  // document.getElementsByClassName('packages')[0].style.overflow='visible';
+  }
+document.getElementById('demo-link').onmouseleave=function(){
+  // document.getElementById('demo-modal').style.height='0';
+  document.getElementById('demo-modal').style.display="none";
+}
+
+
+
+
+
+// Nav JS End
+
+
+
+
+document.getElementsByClassName('section-8-containers')[0].addEventListener('mouseover',function(){
+  document.getElementsByClassName('filled-circle-1')[0].style.fontSize="14px";
+  document.getElementsByClassName('dummy-circle-image-1')[0].style.transform="scale(0.9)";
+  document.getElementsByClassName('section-8-circle-image-1')[0].style.border="2px dashed #fa9db7";
+})
+
+document.getElementsByClassName('section-8-containers')[0].addEventListener('mouseleave',function(){
+  document.getElementsByClassName('filled-circle-1')[0].style.fontSize="0px";
+  document.getElementsByClassName('dummy-circle-image-1')[0].style.transform="scale(1)";
+  document.getElementsByClassName('section-8-circle-image-1')[0].style.border="2px solid #fa9db7";
+})
+
+// 222222222222
+
+document.getElementsByClassName('section-8-containers')[1].addEventListener('mouseover',function(){
+  document.getElementsByClassName('filled-circle-1')[1].style.fontSize="14px";
+  document.getElementsByClassName('dummy-circle-image-2')[0].style.transform="scale(0.9)";
+  document.getElementsByClassName('section-8-circle-image-2')[0].style.border="2px dashed #fa9db7";
+})
+
+document.getElementsByClassName('section-8-containers')[1].addEventListener('mouseleave',function(){
+  document.getElementsByClassName('filled-circle-1')[1].style.fontSize="0px";
+  document.getElementsByClassName('dummy-circle-image-2')[0].style.transform="scale(1)";
+  document.getElementsByClassName('section-8-circle-image-2')[0].style.border="2px solid #fa9db7";
+})
+
+// 3333333333333333333
+
+document.getElementsByClassName('section-8-containers')[2].addEventListener('mouseover',function(){
+  document.getElementsByClassName('filled-circle-1')[2].style.fontSize="14px";
+  document.getElementsByClassName('dummy-circle-image-3')[0].style.transform="scale(0.9)";
+  document.getElementsByClassName('section-8-circle-image-1')[1].style.border="2px dashed #fa9db7";
+})
+
+document.getElementsByClassName('section-8-containers')[2].addEventListener('mouseleave',function(){
+  document.getElementsByClassName('filled-circle-1')[2].style.fontSize="0px";
+  document.getElementsByClassName('dummy-circle-image-3')[0].style.transform="scale(1)";
+  document.getElementsByClassName('section-8-circle-image-1')[1].style.border="2px solid #fa9db7";
+})
+
+// 4444444444444
+
+document.getElementsByClassName('section-8-containers')[3].addEventListener('mouseover',function(){
+  document.getElementsByClassName('filled-circle-1')[3].style.fontSize="14px";
+  document.getElementsByClassName('dummy-circle-image-4')[0].style.transform="scale(0.9)";
+  document.getElementsByClassName('section-8-circle-image-2')[1].style.border="2px dashed #fa9db7";
+})
+
+document.getElementsByClassName('section-8-containers')[3].addEventListener('mouseleave',function(){
+  document.getElementsByClassName('filled-circle-1')[3].style.fontSize="0px";
+  document.getElementsByClassName('dummy-circle-image-4')[0].style.transform="scale(1)";
+  document.getElementsByClassName('section-8-circle-image-2')[1].style.border="2px solid #fa9db7";
+})
+
+
+
+
+
